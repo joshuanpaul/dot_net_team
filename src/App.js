d26ce62f819937df721abffc710ea1e5ff707654
Joshua N Paul <joshuasurya3@gmail.com>
2024-06-20 16:33:35 +0530

    Modifed by Josh

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
-import './App.css';
+
+import Login from './Login';
+import Dashboard from './Dashboard';
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         
-        </p>
-        <a
-          className="App-link"
-         
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         Hi This is New Page
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route  exact path="/" element={<Login/>} />
+          <Route path="/home" element={<Dashboard/>}/>
+          {/* <Route path="/About" element={<About/>}/> */}
+          <Route path="*" element={<Login/>}/>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
